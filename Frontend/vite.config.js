d9d6bdaa4bd33851783113af76d7fcf7bd17a185
Sergio Beleño <sergiosabh05@gmail.com>
2023-07-03 16:22:47 -0500

    default config

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,12 +4,4 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/socket.io": {
-        target: "http://3.134.238.10:10000",
-        ws: true
-      },
-    },
-  },
 });
